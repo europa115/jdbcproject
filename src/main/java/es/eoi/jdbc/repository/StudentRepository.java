7eb4993e5c95206c1f70f2a0b34cfe11f41a7ded
europa115 <estebanto92@gmail.com>
2022-05-27 20:08:42 +0200

    Construyendo paquetes y clases e interfaces

--- src/main/java/es/eoi/jdbc/repository/StudentRepository.java
+++ src/main/java/es/eoi/jdbc/repository/StudentRepository.java
@@ -1,15 +1,22 @@
 package es.eoi.jdbc.repository;
 
+import es.eoi.jdbc.entity.Student;
+
+import java.sql.Connection;
+import java.util.List;
 
 
 public interface StudentRepository {
 
-    /*private java.sql.Connection openConnection(){}
-    public Student findById(String id)
-    public List<Student> findAll()
-    public Student create(Student)
-    public boolean delete(String id)
-    public Student update(Student)*/
+   private Connection openConnection(){
+
+       return  null;
+   }
+    public Student findById(String id);
+    public List<Student> findAll();
+    public Student create(Student student);
+    public boolean delete(String id);
+    public Student update(Student student);
 
 
 }
--- src/main/java/es/eoi/jdbc/repository/StudentRepositoryImpl.java
+++ src/main/java/es/eoi/jdbc/repository/StudentRepositoryImpl.java
@@ -1,4 +1,32 @@
 package es.eoi.jdbc.repository;
 
+import es.eoi.jdbc.entity.Student;
+
+import java.util.List;
+
 public class StudentRepositoryImpl implements StudentRepository{
+    @Override
+    public Student findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public Student create(Student student) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public Student update(Student student) {
+        return null;
+    }
 }
